2cb8d268c658b7928bf5833b2a807d49760f6f47
tokudayusuke <okraysk@gmail.com>
2021-07-06 13:42:17 +0900

    修正2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
   before_action :contributor_confirmation, only: :index
-  before_action :set_order, only: [:index, :create]
+  before_action :set_product, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -30,7 +30,7 @@ class OrdersController < ApplicationController
     redirect_to root_path if current_user == @product.user || @product.order.present?
   end
 
-  def set_order
+  def set_product
     @product = Product.find(params[:product_id])
   end
 
